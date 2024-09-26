7724fd03ff036dd0b9acd04dc01792d7e8dd4be4
Clément <clement.boutet@iut-dijon.u-bourgogne.fr>
2024-09-26 20:26:32 +0200

    test fonctionnel

---
+++ Pipeline/Logic/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Logic
+{
+    public class Person
+    {
+        private string name;
+        private string fname;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public string Fname { get => fname; set => fname = value; }
+        public int Age { get => age; set => age = value; }
+
+
+        public Person(string name, string fname, int age)
+        {
+            this.name = name;
+            this.fname = fname;
+            this.age = age;
+        }
+    }
+}
---
+++ Pipeline/Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Logic;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Person person = new Person("Boutet", "Clément", 20);
+            person.Age = 19;
+            Assert.Equal(person.Age, 19);
+        
+        }
+    }
+}
